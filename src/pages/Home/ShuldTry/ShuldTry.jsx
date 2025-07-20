2555f505de3d91084441cd1fb6342c5e17b27bc5
Akash <hafejmohammadakash@gmail.com>
2025-07-20 16:07:25 +0600

    should try car banaor kaj ses holo

--- src/pages/Home/ShuldTry/ShuldTry.jsx
+++ src/pages/Home/ShuldTry/ShuldTry.jsx
@@ -19,7 +19,7 @@ const ShuldTry = () => {
             <SectionTitle subHeading={"Should Try"} heading={'CHEF RECOMMENDS'} />
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
                 {
-                    items.slice(0, 2).map(item => <ShuoldTryCard key={item._id} item={item} />)
+                    items.slice(0, 3).map(item => <ShuoldTryCard key={item._id} item={item} />)
                 }
             </div>
         </div>
--- src/pages/Home/ShuldTry/ShuoldTryCard.jsx
+++ src/pages/Home/ShuldTry/ShuoldTryCard.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const ShuoldTryCard = ({item}) => {
-    console.log(item)
-    return (
-        <div>
-            
+const ShuoldTryCard = ({ item }) => {
+
+    const {name, image, recipe} = item;
+
+  return (
+    <div className="card bg-base-100  shadow-xl">
+      <figure className="">
+        <img
+          src={image}
+          alt="Shoes"
+          className="rounded-xl w-full"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="card-actions">
+          <button className="btn text-yellow-600 border-b-2 border-b-yellow-500">ADD TO CARD</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ShuoldTryCard;
