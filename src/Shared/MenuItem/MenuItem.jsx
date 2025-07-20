28bee1a47702a1de59f69fdb788b4cb3dbb6b073
Akash <hafejmohammadakash@gmail.com>
2025-07-20 15:27:40 +0600

    menu iewm ard er kaj coltese

---
+++ src/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,12 @@
+
+
+const MenuItem = ({item}) => {
+    console.log(item)
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MenuItem;
--- src/Shared/PopulerMenu/PopulerMenu.jsx
+++ src/Shared/PopulerMenu/PopulerMenu.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SectionTitle from "../../components/SectionTitle";
+import MenuItem from "../MenuItem/MenuItem";
 
 const PopulerMenu = () => {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const filterByData = data.filter((item) => item.category === "popular");
+        setMenu(filterByData);
+      });
+  }, []);
+
   return (
     <section>
       <SectionTitle
@@ -9,8 +21,12 @@ const PopulerMenu = () => {
         heading={"Populer Items"}
       ></SectionTitle>
 
-
-      
+      {/*  */}
+      <div>
+        {
+            menu.map(item => <MenuItem key={item._id} item={item} />)
+        }
+      </div>
     </section>
   );
 };
