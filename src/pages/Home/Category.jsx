565b786346a004b370dd17c4cc541aaea7f45888
Akash <hafejmohammadakash@gmail.com>
2025-07-20 15:09:57 +0600

    section title component done

--- src/pages/Home/Category.jsx
+++ src/pages/Home/Category.jsx
@@ -4,49 +4,60 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // slide category
-import slide1 from'../../assets/home/slide1.jpg'
-import slide2 from'../../assets/home/slide2.jpg'
-import slide3 from'../../assets/home/slide3.jpg'
-import slide4 from'../../assets/home/slide4.jpg'
-import slide5 from'../../assets/home/slide5.jpg'
-
-
+import slide1 from "../../assets/home/slide1.jpg";
+import slide2 from "../../assets/home/slide2.jpg";
+import slide3 from "../../assets/home/slide3.jpg";
+import slide4 from "../../assets/home/slide4.jpg";
+import slide5 from "../../assets/home/slide5.jpg";
 
 import { Pagination } from "swiper/modules";
+import SectionTitle from "../../components/SectionTitle";
 
 const Category = () => {
   return (
-    <Swiper
-      slidesPerView={4}
-      spaceBetween={30}
-      centeredSlides={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper my-20"
-    >
-      <SwiperSlide>
-        <img src={slide1} alt="" />
-        <h3 className="text-3xl text-white uppercase  pl-20 -mt-12">Salad</h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide2} alt="" />
-        <h3 className="text-3xl text-white uppercase  pl-20 -mt-12">Pizza</h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide3} alt="" />
-        <h3 className="text-3xl text-white  uppercase  pl-20 -mt-12">Soups</h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide4} alt="" />
-        <h3 className="text-3xl text-white uppercase  pl-20 -mt-12">Desserts</h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide5} alt="" />
-        <h3 className="text-3xl text-white uppercase  pl-20 -mt-12">Salad</h3>
-      </SwiperSlide>
-    </Swiper>
+    <section>
+        <SectionTitle
+            subHeading={'From 11.00am to 10.00pm'}
+            heading={'Order Online'}
+        ></SectionTitle>
+
+
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-20"
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+          <h3 className="text-3xl text-white uppercase  pl-20 -mt-12">Salad</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+          <h3 className="text-3xl text-white uppercase  pl-20 -mt-12">Pizza</h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+          <h3 className="text-3xl text-white  uppercase  pl-20 -mt-12">
+            Soups
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+          <h3 className="text-3xl text-white uppercase  pl-20 -mt-12">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+          <h3 className="text-3xl text-white uppercase  pl-20 -mt-12">Salad</h3>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
