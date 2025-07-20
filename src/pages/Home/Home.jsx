04891675b670020844d67b1c89691a6c1748eb56
Akash <hafejmohammadakash@gmail.com>
2025-07-20 13:17:04 +0600

    swiper categori

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Banner from "./Banner";
+import Category from "./Category";
 
 
 const Home = () => {
     return (
         <div>
             <Banner />
+
+            <Category />
         </div>
     );
 };
