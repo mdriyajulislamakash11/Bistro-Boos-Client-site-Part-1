2e849fa94e0d21bd5131e5c6376da2878da56a99
Akash <hafejmohammadakash@gmail.com>
2025-07-20 12:17:41 +0600

    nabvar er kaj

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../Shared/Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Outlet />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
