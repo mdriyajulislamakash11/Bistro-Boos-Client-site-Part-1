4e93a573f6979e3fd71ec13f4cc40bb0dc31ea48
Akash <hafejmohammadakash@gmail.com>
2025-07-22 00:21:49 +0600

    tabbs er kaj suru

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
-import Cover from '../../Shared/cover/Cover';
+import { useState } from "react";
+import Cover from "../../Shared/cover/Cover";
 
-
-import orderCover from'../../assets/shop/banner2.jpg'
+import orderCover from "../../assets/shop/banner2.jpg";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const Order = () => {
-    return (
+  const [tabIndex, setTabIndex] = useState(0);
+
+  return (
+    <div>
+      <Cover
+        img={orderCover}
+        title="Order Food"
+        description="Authoritatively predominate client-centric products without adaptive ROI. Enthusiastically pursue user friendly alignments via excellent value. Quickly facilitate."
+      ></Cover>
+
+
+      {/* Tabs */}
+
+      <div className="flex justify-center mt-16 mb-8">
         <div>
-            <Cover img={orderCover} title="Order Food" description="Authoritatively predominate client-centric products without adaptive ROI. Enthusiastically pursue user friendly alignments via excellent value. Quickly facilitate."></Cover>
-            
+          <Tabs
+            defaultIndex={tabIndex}
+            onSelect={(index) => setTabIndex(index)}
+          >
+            <TabList>
+              <Tab>Salad</Tab>
+              <Tab>Pizza</Tab>
+              <Tab>Soups</Tab>
+              <Tab>Desserts</Tab>
+              <Tab>Drinks</Tab>
+            </TabList>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+          </Tabs>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Order;
