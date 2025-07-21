3c6b7a1829392c88e334c4a1192b8a8bc7755c41
Akash <hafejmohammadakash@gmail.com>
2025-07-21 23:07:23 +0600

    food order page er kaj suru

--- src/Shared/cover/Cover.jsx
+++ src/Shared/cover/Cover.jsx
@@ -12,7 +12,7 @@ const Cover = ({ img, title, description }) => {
       <div className="hero-overlay bg-opacity- "></div>
       <div className="hero-content text-neutral-content text-center border py-12 px-56 text-white bg-black bg-opacity-60">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold uppercase font-light">{title}</h1>
+          <h1 className="mb-5 text-5xl uppercase font-light">{title}</h1>
           <p className="mb-5 uppercase">
             {description}
           </p>
--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Cover from '../../Shared/cover/Cover';
+
+
+import orderCover from'../../assets/shop/banner2.jpg'
 
 const Order = () => {
     return (
         <div>
+            <Cover img={orderCover} title="Order Food" description="Authoritatively predominate client-centric products without adaptive ROI. Enthusiastically pursue user friendly alignments via excellent value. Quickly facilitate."></Cover>
             
         </div>
     );
