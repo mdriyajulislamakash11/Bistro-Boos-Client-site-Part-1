4bbb65c88f273aa97cd9c3a59159f9e377520491
Akash <hafejmohammadakash@gmail.com>
2025-07-23 23:38:40 +0600

    user create korlam

---
+++ src/Firebase/AuthProvider.jsx
@@ -0,0 +1,46 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import React, { createContext, useEffect, useState } from "react";
+import { auth } from "./firebase.init";
+
+export const AuthContex = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+      console.log("Observer User: ", currentUser);
+    });
+    return () => {
+      return unSubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    signIn,
+    loading,
+    createUser,
+  };
+
+  return <AuthContex.Provider value={authInfo}>{children}</AuthContex.Provider>;
+};
+
+export default AuthProvider;
