7605db16e1cd10f955d5a1ff7971550e00ad0a17
Akash <hafejmohammadakash@gmail.com>
2025-07-23 21:12:14 +0600

    catcha er kaj kortesi suru holo

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,84 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { LoadCanvasTemplate, loadCaptchaEnginge } from "react-simple-captcha";
+
 
 const Login = () => {
-    return (
-        <div>
-            login
+  useEffect(() => {
+    loadCaptchaEnginge(6); 
+  }, []);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const captchaValue = form.captcha.value;
+
+  };
+
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center md:w-1/2 lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+          <div className="card md:w-1/2 max-w-sm bg-base-100 shadow-2xl">
+            <form onSubmit={handleLogin} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  name="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  name="password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
+              {/* captcha */}
+              <div className="form-control">
+                <label className="label">
+                  <LoadCanvasTemplate />
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter captcha"
+                  name="captcha"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
