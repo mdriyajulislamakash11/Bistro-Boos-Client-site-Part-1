00c0d65cfbd953ce6040477cb0f59c32ba123f3e
Akash <hafejmohammadakash@gmail.com>
2025-07-28 11:07:59 +0600

    user er email add to wachlist er length dekhabo

--- src/hook/useCart.jsx
+++ src/hook/useCart.jsx
@@ -1,24 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
-
-
+import useAuth from "./useAuth";
 
 const useCart = () => {
-    // tan stack query: 
-    const axiosSecure = useAxiosSecure()
+  // tan stack query:
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
 
-    const {data: cart=[]} = useQuery({
-        queryKey: ['cart'],
-        queryFn: async () => {
-            const res = await axiosSecure.get('/carts')
-            return res.data
-        }
-    })
+  const { data: cart = [] } = useQuery({
+    queryKey: ["cart", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/carts?email=${user.email}`);
+      return res.data;
+    },
+  });
 
-    return [cart]
-
-
-
+  return [cart];
 };
 
 export default useCart;
