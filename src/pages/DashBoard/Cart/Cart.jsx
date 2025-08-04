f9ca18ea457bb7486a870cced9ca20437637e7e6
Akash <hafejmohammadakash@gmail.com>
2025-08-04 13:40:11 +0600

    my cart er kaj ses korlam

--- src/pages/DashBoard/Cart/Cart.jsx
+++ src/pages/DashBoard/Cart/Cart.jsx
@@ -1,14 +1,60 @@
 import useCart from "../../../hook/useCart";
 
-
 const Cart = () => {
-    const [cart] = useCart()
-    console.log(cart)
-    return (
-        <div>
-            <h2 className="text-6xl">My Cart</h2>
-        </div>
-    );
+  const [cart] = useCart();
+
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+  return (
+    <div>
+      <div className="flex justify-evenly items-center my-6">
+        <h2 className="text-4xl font-semibold">Items: {cart.length}</h2>
+        <h2 className="text-4xl font-semibold">Total Price: ${totalPrice}</h2>
+        <button className="btn btn-primary">Pay Bill</button>
+      </div>
+
+      {/* table */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img src={item.image} />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <div>
+                    <div className="font-bold">{item.name}</div>
+                  </div>
+                </td>
+                <td>${item.price}</td>
+                <td>
+                  <button className="btn btn-ghost btn-xs">Details</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
