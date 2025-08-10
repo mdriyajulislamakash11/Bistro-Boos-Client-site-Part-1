511de1c9284fa4646bb86dc81327ec55d4d941b9
Akash <hafejmohammadakash@gmail.com>
2025-08-10 10:36:01 +0600

    ekahen all user ana holo

--- src/pages/DashBoard/All Users/AllUsers.jsx
+++ src/pages/DashBoard/All Users/AllUsers.jsx
@@ -1,14 +1,70 @@
-import React from 'react';
+import React from "react";
+import useAxiosSecure from "../../../hook/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import { FaTrashAlt, FaUser } from "react-icons/fa";
 
 const AllUsers = () => {
-    return (
-        <div className=''>
-            <div className='flex justify-evenly my-4'>
-                <h2 className='text-3xl'>All Users: </h2>
-                <h2 className='text-3xl'>Total Users: </h2>
-            </div>
-        </div>
-    );
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const result = await axiosSecure.get(`/users`);
+      return result.data;
+    },
+  });
+
+  const handleDelete = (id) => {
+    console.log(id)
+  }
+
+  return (
+    <div className="p-10">
+      <div className="flex justify-evenly my-4">
+        <h2 className="text-3xl">All Users: </h2>
+        <h2 className="text-3xl">Total Users: </h2>
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                    <button
+                    onClick={() => handleAdminUser(item._id)}
+                    className="bg-orange-400 btn btn-lg"
+                  >
+                    <FaUser className=" " />
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <FaTrashAlt className="text-red-600" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
