72bba06eef2180fa2b1f3f057a6f9ec8585e1f30
Akash <hafejmohammadakash@gmail.com>
2025-08-10 11:04:25 +0600

    ekahen all user er admin button er kaj korlam

--- src/pages/DashBoard/All Users/AllUsers.jsx
+++ src/pages/DashBoard/All Users/AllUsers.jsx
@@ -14,31 +14,49 @@ const AllUsers = () => {
     },
   });
 
+  // user Admin button update:
+  const handleAdminUser = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch()
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} id an Admin Now..!`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
+
+  // User deleted button:
   const handleDelete = (id) => {
     Swal.fire({
-          title: "Are you sure?",
-          text: "You won't be able to revert this!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Yes, delete it!",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            // delete oparetions:
-            axiosSecure.delete(`/users/${id}`).then((res) => {
-              if (res.data.deletedCount > 0) {
-                refetch();
-                Swal.fire({
-                  title: "Deleted!",
-                  text: "Your file has been deleted.",
-                  icon: "success",
-                });
-              }
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // delete oparetions:
+        axiosSecure.delete(`/users/${id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
             });
           }
         });
-  }
+      }
+    });
+  };
 
   return (
     <div className="p-10">
@@ -66,12 +84,12 @@ const AllUsers = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                    <button
-                    onClick={() => handleAdminUser(item._id)}
+                  { user.role === 'admin' ? 'Admin' : <button
+                    onClick={() => handleAdminUser(user)}
                     className="bg-orange-400 btn btn-lg"
                   >
                     <FaUser className=" " />
-                  </button>
+                  </button>}
                 </td>
                 <td>
                   <button
