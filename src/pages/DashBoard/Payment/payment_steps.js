751daaaf8a2291fee149f25a800f9a6546cc88e1
Akash <hafejmohammadakash@gmail.com>
2025-08-12 02:49:07 +0600

    payment method er kaj korlam

--- src/pages/DashBoard/Payment/payment_steps.js
+++ src/pages/DashBoard/Payment/payment_steps.js
@@ -4,5 +4,9 @@
  * 2. create card elemnt
  * 3. create stripe account and get publishable key 
  * 4. use publishable key and use stripe to get card information and error
+ * 5. create peyement intent post on the server. and retun client secret. install stripe on the server side and get client secret. make sure you used the payment method types: ['card]
+ * 6. from client side get the client secret and save it.
+ * 7. use confirm card oayment and pass user information, card and client secret 
+ * 6. display transaction is
  * 
  */
